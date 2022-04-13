620f72f9abf382ffc5cea0fd6163641ce4377203
2Gily <rlfgud91@gmail.com>
2022-04-14 01:04:06 +0900

    START 2022.04.14

---
+++ ProblemSolvingSeason2/16967-배열 복원하기.cpp
@@ -0,0 +1,25 @@
+// Date	 : 2022-04-12
+// BOJ	 : 16967-배열 복원하기
+// Genre : 구현
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#define endl "\n"
+
+using namespace std;
+
+void init()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int main(void)
+{
+	init();
+
+
+	return 0;
+}
---
+++ ProblemSolvingSeason2/Template.cpp
@@ -0,0 +1,25 @@
+// Date	 : 2022-04-12
+// BOJ	 : 
+// Genre : 
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#define endl "\n"
+
+using namespace std;
+
+void init()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int main(void)
+{
+	init();
+
+
+	return 0;
+}
